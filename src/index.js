faddffd11ab87b21902768ac34bfc672267e7d35
Julian David Forero Estrada <julian.forero282@soy.sena.edu.co>
2022-10-26 10:05:49 -0500

    Configuracion 2.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const usuarios = require('./controllers/usuarios');
+const cursos = require('./controllers/cursos');
+
+
 const express = require ('express');
 const mongoose = require ('mongoose');
 
@@ -13,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 //end points (recursos)
-app.use('/api/usuarios', usuarios);
+app.use('/api/usuarios',usuarios);
 app.use('/api/cursos', cursos);
 
 const port = process.env.PORT || 3000;
