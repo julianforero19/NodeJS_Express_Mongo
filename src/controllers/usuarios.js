f2da5c1dbe97136b6b446d233a003051bc6f2c0d
Julian David Forero Estrada <julian.forero282@soy.sena.edu.co>
2022-10-26 09:57:32 -0500

    Configuracion FINAL

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const Usuario = require ('../models/usuario_model');
+const Joi = require('@hapi/joi')
 const ruta = express.Router();
 
 ruta.get('/', (req,res)=>{
