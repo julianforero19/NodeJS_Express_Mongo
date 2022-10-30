17371cc97c41692edfcf4496c880574d45ea15f1
Julian David Forero Estrada <julian.forero282@soy.sena.edu.co>
2022-10-30 12:33:41 -0500

    correccion 3

--- src/controllers/cursos.js
+++ src/controllers/cursos.js
@@ -2,9 +2,7 @@ const express = require('express');
 const Curso = require('../models/curso_model');
 const ruta = express.Router();
 
-ruta.get('/', (req,res)=>{
-    res.json('Respuesta a peticion GET de CURSOS funcionando correctamente...');
-});
+
 
 //Endpoint de tipo POST para el recurso CURSOS
 ruta.post('/',(req,res)=>{
