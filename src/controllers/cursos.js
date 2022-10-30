75ede2309fbccd40d0811896f57bdd49ec0c42ac
Julian David Forero Estrada <julian.forero282@soy.sena.edu.co>
2022-10-30 12:32:23 -0500

    correccion 2

--- src/controllers/cursos.js
+++ src/controllers/cursos.js
@@ -56,4 +56,42 @@ async function actualizarCurso(id, body){
 }
 
 
+
+//Funcion asincrona para inactivar cursos
+async function desactivarCurso(id){
+    let curso = await Curso.findByIdAndUpdate(id,{
+        $set: {
+            estado :false
+        }
+    },{new :true});
+    return curso;
+}
+
+//Endpoint de tipo DELETE para el recurso CURSOS
+ruta.delete('/:id', (req,res)=> {
+    let resultado = desactivarCurso(req.params.id);
+    resultado.then(curso =>{
+        res.json(curso);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
+
+//Funcion asincrona para listar los cursos activos
+async function listarCursosActivos(){
+    let cursos = await Curso.find({"estado":true});
+    return cursos;
+}
+
+//Endpoint de tipo GET para el recurso CURSOS
+ruta.get('/',(req,res)=>{
+    let resultado = listarCursosActivos();
+    resultado.then(cursos=>{
+        res.json(cursos);
+    }).catch(err =>{
+        res.status(400).json(err);
+    })
+});
+
+
 module.exports = ruta;
