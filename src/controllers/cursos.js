8878a760cfc6716188ef003399bea4eec22dff14
Julian David Forero Estrada <julian.forero282@soy.sena.edu.co>
2022-10-30 12:22:09 -0500

    controler curso con todos los metodos

--- src/controllers/cursos.js
+++ src/controllers/cursos.js
@@ -6,4 +6,54 @@ ruta.get('/', (req,res)=>{
     res.json('Respuesta a peticion GET de CURSOS funcionando correctamente...');
 });
 
+//Endpoint de tipo POST para el recurso CURSOS
+ruta.post('/',(req,res)=>{
+    let resultado = crearCurso(req.body);
+
+    resultado.then(curso =>{
+        res.json({
+            curso
+        })
+    }).catch(err=>{
+        res.status(400).json({
+            err
+        })
+    })
+});
+
+//Funcion asincrona para crear cursos
+async function crearCurso(body){
+    let curso = new Curso({
+        titulo       : body.titulo,
+        descripcion  : body.descripcion,
+        alumnos      : body.alumnos,
+        calificacion : body.calificacion
+    });
+    return await curso.save();
+}
+
+
+
+//Endpoint de tipo PUT para el recurso CURSOS
+ruta.put('/:id', (req,res)=>{
+    let resultado = actualizarCurso(req.params.id, req.body);
+    resultado.then(curso =>{
+        res.json(curso)
+    }).catch(err =>{
+        res.status(400).json(err)
+    })
+})
+
+//Funcion asincrona para actualizar cursos
+async function actualizarCurso(id, body){
+    let curso = await Curso.findByIdAndUpdate(id,{
+        $set : {
+            titulo: body.titulo,
+            descripcion: body.descripcion
+        }
+    }, {new: true});
+    return curso;
+}
+
+
 module.exports = ruta;
